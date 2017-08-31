3cd8bf5dc5f7f2770cffce6f053426505e2b2839
DESKTOP-G7653J7\Jangwon <wlckd1031@sju.ac.kr>
2017-08-31 20:21:13 +0900

    AccountTransfer, MenuActivity, ProductPurchaseContent, RegisterActivity, SplashActivty 생성

--- NFCAccount/app/src/main/java/com/example/woga1/nfcaccount/ProductPurchaseContent.java
+++ NFCAccount/app/src/main/java/com/example/woga1/nfcaccount/ProductPurchaseContent.java
@@ -3,13 +3,11 @@ package com.example.woga1.nfcaccount;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ProductPurchaseContent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        
+        setContentView(R.layout.activity_product_purchase_content);
     }
 }
