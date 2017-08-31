21f00b1045f90b722674f2c36d2a92e9ac63576f
DESKTOP-G7653J7\Jangwon <wlckd1031@sju.ac.kr>
2017-08-31 21:13:20 +0900

    로그인 화면

---
+++ NFCAccount/app/src/main/java/com/example/woga1/nfcaccount/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.woga1.nfcaccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
