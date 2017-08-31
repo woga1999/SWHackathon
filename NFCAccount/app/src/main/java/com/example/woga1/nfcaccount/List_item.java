4039533077c409a654c6df7d1dee1903bea03674
DESKTOP-G7653J7\Jangwon <wlckd1031@sju.ac.kr>
2017-09-01 04:27:51 +0900

    MenuActivity List를 custom Listview로 만듬

---
+++ NFCAccount/app/src/main/java/com/example/woga1/nfcaccount/List_item.java
@@ -0,0 +1,35 @@
+package com.example.woga1.nfcaccount;
+
+/**
+ * Created by Jangwon on 2017-09-01.
+ */
+
+public class List_item {
+
+    private String seller ;
+    private String money ;
+    private String address ;
+
+    public void setSeller(String sellerParam) {
+        seller = sellerParam ;
+    }
+    public void setMoney(String moneyParam) {
+        money = moneyParam ;
+    }
+    public void setAddress(String addressParam) {
+        address = addressParam ;
+    }
+
+    public String getSeller() {
+        return this.seller ;
+    }
+    public String getMoney() {
+        return this.money ;
+    }
+
+    public String getAddress() {
+        return this.address ;
+    }
+
+
+}
--- NFCAccount/app/src/main/java/com/example/woga1/nfcaccount/MenuActivity.java
+++ NFCAccount/app/src/main/java/com/example/woga1/nfcaccount/MenuActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,21 +18,30 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        final String[] sellerName = {"손장원","최종원"};
+//        final String[] sellerName = {"손장원","최종원"};
 
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFFFFFFFF));
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.custom_menubar);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, sellerName) ;
+//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, sellerName) ;
 
         ListView listview = (ListView) findViewById(R.id.purchaseList) ;
-        listview.setAdapter(adapter) ;
+        final ListviewAdapter listViewAdapter = new ListviewAdapter();
+
+        listViewAdapter.addItem("신재혁치킨","3600원", "서울시 광진구 군자동 세종대학교") ;
+        listViewAdapter.addItem("최종원떡볶이","3600원", "서울시 관악구 낙성대 종원이네") ;
+        listview.setAdapter(listViewAdapter) ;
+//        listViewAdapter.addItem("박효완스","3600원", "서울시 광진구 뚝섬유원지역") ;
+
+
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView parent, View v, int position, long id) {
-                if(sellerName[position].equals(""))
+//                if(sellerName[position].equals(""))
+                Toast.makeText(getApplicationContext(),listViewAdapter.getItem(position).toString(),Toast.LENGTH_SHORT).show();
+                if(listViewAdapter.getItem(position).equals(""))
                 {
                     Toast.makeText(getApplicationContext(),"클릭할게 없다",Toast.LENGTH_SHORT).show();
                     Log.e("position", "빈칸");
